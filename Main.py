d7901e6c13f4492f1a10649c09ec55dbaf792931
Ambattz <alvingeorge30@gmail.com>
2021-04-02 21:30:06 +0530

    5sec gitignored

--- Main.py
+++ Main.py
@@ -33,11 +33,13 @@ net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
 # (note: normalization is done via the authors of the MobileNet SSD
 # implementation)
 
+
 def itms():
     def density(image):
         count = 0
         (h, w) = image.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+        blob = cv2.dnn.blobFromImage(cv2.resize(
+            image, (300, 300)), 0.007843, (300, 300), 127.5)
 
         # pass the blob through the neural network
         net.setInput(blob)
@@ -49,56 +51,55 @@ def itms():
             confidence = detections[0, 0, i, 2]
 
             # filter out weak detections by ensuring the 'confidence' is greater than the minimum confidence
-                # extract the index of the classes label from the 'detections',
-                # then compute the (x, y)-coordinates of the bounding box for the object
+            # extract the index of the classes label from the 'detections',
+            # then compute the (x, y)-coordinates of the bounding box for the object
             if confidence > 0.000000000000001:
                 idx = int(detections[0, 0, i, 1])
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype('int')
 
-                    # display the prediction
+                # display the prediction
                 label = '{}: {:.2f}%'.format(CLASSES[idx], confidence * 100)
                 count = count + 1
-                cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+                cv2.rectangle(image, (startX, startY),
+                              (endX, endY), COLORS[idx], 2)
                 y = startY - 15 if startY - 15 > 15 else startY + 15
-                cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                cv2.putText(image, label, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
         return count
 
     def timing(x):
-            t=0
-            if x>30:
-                t=60
-            elif (x>20 and x<30):
-                t=40
-            elif (x<20 and x>10):
-                t=30
-            else:
-                t=20
-            return t
+        t = 0
+        if x > 30:
+            t = 60
+        elif (x > 20 and x < 30):
+            t = 40
+        elif (x < 20 and x > 10):
+            t = 30
+        else:
+            t = 5
+        return t
 
-
-    outputFile = "output\ result1.png"
-    outputFile1 = "output\ result2.png"
-    outputFile2 = "output\ result3.png"
-    outputFile3 = "output\ result4.png"
+    outputFile = "output/result1.png"
+    outputFile1 = "output/result2.png"
+    outputFile2 = "output/result3.png"
+    outputFile3 = "output/result4.png"
 
     cap1 = cv2.imread("input/lane1.png")
     c1 = density(cap1)
-    cv2.imwrite(outputFile,cap1)
-
+    cv2.imwrite(outputFile, cap1)
 
     cap2 = cv2.imread("input/lane2.png")
     c2 = density(cap2)
-    cv2.imwrite(outputFile,cap2)
+    cv2.imwrite(outputFile1, cap2)
 
     cap3 = cv2.imread("input/lane3.png")
     c3 = density(cap3)
-    cv2.imwrite(outputFile,cap3)
+    cv2.imwrite(outputFile2, cap3)
 
     cap4 = cv2.imread("input/lane4.png")
     c4 = density(cap4)
-    cv2.imwrite(outputFile,cap4)
-    
+    cv2.imwrite(outputFile3, cap4)
 
     print("Vehicle Count lane no 1 " + str(c1))
     t1 = timing(c1)
@@ -140,5 +141,6 @@ def itms():
     print("")
 
     itms()
+
 
 itms()
